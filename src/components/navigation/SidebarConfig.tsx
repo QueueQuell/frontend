ee021a3b9f06d00d0a647381f47d6d2bb1504cc9
Queue Quell <17708554+iam-aki@users.noreply.github.com>
2026-04-12 00:05:20 +0530

    List page moved to root of their own module

--- src/components/navigation/SidebarConfig.tsx
+++ src/components/navigation/SidebarConfig.tsx
@@ -73,7 +73,7 @@ const ALL_SIDEBAR_ITEMS: SidebarItem[] = [
     subItems: createSubItems("/catalog", [
       ["Items", "/items/list"],
       ["Create Item", "/items/create"],
-      ["Categories", "/categories/list"],
+      ["Categories", "/categories"],
     ]),
   },
   {
@@ -152,8 +152,8 @@ const ALL_SIDEBAR_ITEMS: SidebarItem[] = [
     icon: <AdminPanelSettingsIcon />,
     href: "/administrator",
     subItems: createSubItems("/administrator", [
-      ["Organisations", "/organisations/list"],
-      ["Users", "/users/list"],
+      ["Organisations", "/organisations"],
+      ["Users", "/users"],
     ]),
     adminOnly: true,
   },
